4d134aafb82560f281f62eaea8ab409111b2473f
K To <kto@pop-os.localdomain>
2023-03-21 15:44:35 -0700

    post request

--- CLIENT/src/pages/LoginPage.tsx
+++ CLIENT/src/pages/LoginPage.tsx
@@ -1,19 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../assets/LoginPage.css'
+import axios from 'axios'
 
 function LoginPage() {
+    const [username, setUsername] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [message, setMessage] = useState<string>("");
+
+    let handleSubmit = async (e) => {
+        e.preventDefault();
+        axios
+            .post('http://localhost:3000/', { 
+                username: username, 
+                password: password 
+            })
+            .then((res) => {
+                setUsername("");
+                setPassword("");
+                setMessage("User logged in successfully!");
+            }
+            )
+            .catch((err) => {
+                console.error(err);
+            });
+    }
 
     return (
         <div className="center">
             <h1>Login</h1>
-            <form method="post">
+            <form onSubmit={handleSubmit}>
+                <div>{message ? <p>{message}</p> : null}</div>
                 <div className="txt_field">
-                    <input type="text" required />
+                    <input onChange={(e) => setUsername(e.target.value)} value={username} type="text" required />
                     <span></span>
                     <label>Username</label>
                 </div>
                 <div className="txt_field">
-                    <input type="password" required />
+                    <input onChange={(e) => setPassword(e.target.value)} value={password} type="password" required />
                     <span></span>
                     <label>Password</label>
                 </div>
--- SERVER/server.js
+++ SERVER/server.js
@@ -1,9 +1,22 @@
 import express from 'express'
+import bcrypt from 'bcryptjs'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(bodyParser.json())
+app.use(cors({origin: true, credentials: true}))
+
+app.get('/', function(req, res) {
+  res.send('Hello!')
+  console.log('Hello World!')
+})
+
+app.post('/', function (req, res) {
+  console.log(req.body);
+  res.send('Hello!')
 })
 
 app.listen(port, () => {
